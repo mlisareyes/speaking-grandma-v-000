ca69bd3e250f1707572454dbd0835ae92c2f1542
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-07 06:30:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif string.upcase
-    return "NO, NOT SINCE 1938!"
-  else string == "I LOVE YOU GRANDMA!"
+  elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else string.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
